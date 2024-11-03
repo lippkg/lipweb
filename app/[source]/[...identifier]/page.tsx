c46000b57643d7c22135cbf3066b70c82e22689a
QingYu <62042544+dreamguxiang@users.noreply.github.com>
2024-11-03 22:38:06 +0800

    fix: try fix

--- app/[source]/[...identifier]/page.tsx
+++ app/[source]/[...identifier]/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { tryGetPackage } from "@/lib/api";
 import { fetchReadme } from "@/lib/readme-fetcher";
 import { redirect } from "next/navigation";
@@ -17,41 +16,35 @@ import { motion } from "framer-motion";
 
 export default function Page({
   params,
-}: Readonly<{
+}: {
   params: { source: string; identifier: string[] };
-}>) {
-  const [data, setData] = useState<{
-    pkg?: GetPackageResponse;
-    readme?: any;
-    error?: boolean;
-    tab: string;
-    loading: boolean;
-  }>({
-    tab:
-      params.identifier.length === 3 && params.identifier[2] === "versions"
-        ? "versions"
-        : "description",
-    loading: true,
-  });
+}) {
+  const [pkg, setPkg] = useState<GetPackageResponse | undefined>(undefined);
+  const [readme, setReadme] = useState<any>(undefined);
+  const [error, setError] = useState<boolean | undefined>(undefined);
+  const [tab, setTab] = useState<string>(
+    params.identifier.length === 3 && params.identifier[2] === "versions"
+      ? "versions"
+      : "description"
+  );
+  const [loading, setLoading] = useState<boolean>(true);
 
   const fetchData = async () => {
     if (params.identifier.length >= 2) {
       const identifier = params.identifier.slice(0, 2).join("/");
       const response = await tryGetPackage(params.source, identifier);
       if (response.err) {
-        setData((prev) => ({ ...prev, error: true, loading: false }));
+        setError(true);
+        setLoading(false);
         redirect("/404");
       } else {
         const readmeData = await fetchReadme(
           params.source as "github" | "pypi",
           identifier
         );
-        setData((prev) => ({
-          ...prev,
-          pkg: response.val,
-          readme: readmeData,
-          loading: false,
-        }));
+        setPkg(response.val);
+        setReadme(readmeData);
+        setLoading(false);
       }
     }
   };
@@ -60,12 +53,12 @@ export default function Page({
     fetchData();
   }, [params]);
 
-  const filteredTags = data.pkg?.tags.filter(
+  const filteredTags = pkg?.tags.filter(
     (item) => !item.startsWith("type:") && !item.startsWith("platform:")
   );
 
   const handleTabChange = (key: string) => {
-    setData((prev) => ({ ...prev, tab: key }));
+    setTab(key);
     const basePath = `/github/${params.identifier[0]}/${params.identifier[1]}`;
     const newUrl = key === "description" ? basePath : `${basePath}/${key}`;
     window.history.pushState(null, "", newUrl);
@@ -73,8 +66,8 @@ export default function Page({
 
   return (
     <div className="container mx-auto max-w-7xl px-6 flex-grow">
-      {data.loading ? (
-        <div className="flex items-center justify-center h-screen">
+      {loading ? (
+        <div className="flex items-center justify-center">
           <Spinner label="Loading" size="lg" color="default" />
         </div>
       ) : (
@@ -89,7 +82,7 @@ export default function Page({
               <div className="flex-shrink-0 p-4">
                 <Image
                   src={
-                    data.pkg?.avatarUrl ||
+                    pkg?.avatarUrl ||
                     "https://stickerly.pstatic.net/sticker_pack/8cP1NeB69qFawu3Cn0vA/SL4DEZ/20/5e094b29-a0b9-4c95-8359-71af47910afb.png"
                   }
                   className="rounded-3xl"
@@ -102,18 +95,17 @@ export default function Page({
                 <div className="flex flex-col h-full">
                   <div className="mb-2">
                     <h3 className="font-semibold text-foreground/90">
-                      <span className="text-large"> {data.pkg?.name}</span>
+                      <span className="text-large">{pkg?.name}</span>
                     </h3>
                     <p className="text-medium text-foreground/80">
-                      {data.pkg?.description}
+                      {pkg?.description}
                     </p>
                   </div>
                   <div className="mt-2 flex text-gray-500 items-center">
-                    <div className="flex items-center gap-2  pr-4 font-semibold dark:border-gray-600 dark:text-gray-400">
+                    <div className="flex items-center gap-2 pr-4 font-semibold dark:border-gray-600 dark:text-gray-400">
                       <FaStar />
-                      <span>{data.pkg?.hotness}</span>
+                      <span>{pkg?.hotness}</span>
                     </div>
-
                     {(filteredTags?.length || -1) > 0 && (
                       <div className="flex items-center gap-2 md:pl-4 hidden md:flex">
                         <FaTag className="text-gray-500" />
@@ -132,18 +124,16 @@ export default function Page({
                   </div>
                 </div>
               </div>
-              <div className="flex-shrink-0  p-4 min-w-[150px]"></div>
+              <div className="flex-shrink-0 p-4 min-w-[150px]"></div>
             </motion.div>
-
             <hr className="border-foreground/20" />
-
             <div className="flex flex-col lg:flex-row lg:space-x-4">
               <div className="flex-1 min-w-0">
                 <Tabs
                   key="underlined"
                   variant="underlined"
                   aria-label="Tabs variants"
-                  selectedKey={data.tab}
+                  selectedKey={tab}
                   onSelectionChange={(key) => handleTabChange(key as string)}
                 >
                   <Tab key="description" title="Description">
@@ -155,8 +145,8 @@ export default function Page({
                       <Card className="shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300">
                         <div className="tab-content">
                           <Readme
-                            readme={data.readme}
-                            pkg={data.pkg || ({} as GetPackageResponse)}
+                            readme={readme}
+                            pkg={pkg || ({} as GetPackageResponse)}
                           />
                         </div>
                       </Card>
@@ -182,7 +172,7 @@ export default function Page({
                       "https://" +
                       params.source +
                       ".com/" +
-                      data.pkg?.identifier +
+                      pkg?.identifier +
                       "/issues"
                     }
                   />
@@ -194,10 +184,7 @@ export default function Page({
                       </span>
                     }
                     href={
-                      "https://" +
-                      params.source +
-                      ".com/" +
-                      data.pkg?.identifier
+                      "https://" + params.source + ".com/" + pkg?.identifier
                     }
                   />
                 </Tabs>
@@ -208,7 +195,7 @@ export default function Page({
                   animate={{ opacity: 1 }}
                   transition={{ duration: 1, delay: 0.2 }}
                 >
-                  <Card className=" shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300">
+                  <Card className="shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300">
                     preset1
                   </Card>
                 </motion.div>
@@ -217,7 +204,7 @@ export default function Page({
                   animate={{ opacity: 1 }}
                   transition={{ duration: 1, delay: 0.4 }}
                 >
-                  <Card className=" shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300">
+                  <Card className="shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300">
                     preset2
                   </Card>
                 </motion.div>
